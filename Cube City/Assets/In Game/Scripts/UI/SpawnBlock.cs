e7810547d604ba5856087dd75d570dfb5c1a53f4
Cecil Thomas <Cecil Thomas>
2016-12-03 21:35:11 -0700

    UI and Block Prefabs

---
+++ Cube City/Assets/In Game/Scripts/UI/SpawnBlock.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnBlock : MonoBehaviour 
+{
+	public GameObject block;
+
+	public void CreateBlock()
+	{
+		Instantiate(block, new Vector3(0, 0, 0), Quaternion.identity);
+	}
+}
